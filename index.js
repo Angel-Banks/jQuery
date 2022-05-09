8401cf81c048bae6b2e2953545a9b891fcf532ed
Angel-Banks <angelonuchuks@gmail.com>
2022-05-10 00:09:02 +0100

    updated

---
+++ index.js
@@ -0,0 +1,41 @@
+
+// console.log($('h1').css('color'));
+
+// console.log($('h1').css('font-size'));
+
+
+// $('h1').css('color', 'red');
+
+// $("h1").addClass("big-title margin-50");
+
+// $("h1").addClass("big-title margin-50");
+//
+// console.log($("h1").css ('margin'));
+
+// $("h1").removeClass("big-title");
+
+// $("h1").addClass("big-title");
+// $ ("h1").text ("oops");
+
+// $ ("button").text ("don't click me");
+
+// $ ("button").html ("<em>hey<em>");
+// console.log($ ('img').attr ("src"));
+//
+// $ ("a").attr ("href", "https://www.yahoo.com");
+//
+// console.log($ ('h1').attr ('class'));
+
+// $("h1").click(function(){
+//   $('h1').css('color', 'red');
+// });
+
+// for (var i=0;i<5; i++) {
+//   document.querySelectorAll ("button")[i].addEventListener ("click", function() {
+//     document.querySelector("h1").style.color ="purple";
+//   });
+// }
+
+$ ("button").click(function (){
+  $ ('h1').css ("color","red");
+});
